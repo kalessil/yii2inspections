5889354800d892089a09410c9d7bfc21b6712b8f
Vladimir Reznichenko <kalessil@gmail.com>
2017-01-02 14:21:31 +0100

    [#1] Added plugin settings

---
+++ src/main/com/kalessil/phpStorm/yii2inspections/Yii2InspectionsProjectConfigPanel.java
@@ -0,0 +1,24 @@
+package com.kalessil.phpStorm.yii2inspections;
+
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.awt.*;
+
+/*
+ * This file is part of the Yii2 Inspections package.
+ *
+ * Author: Vladimir Reznichenko <kalessil@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+public class Yii2InspectionsProjectConfigPanel extends JPanel {
+    public Yii2InspectionsProjectConfigPanel(@NotNull final Project project) {
+        super(new BorderLayout());
+
+        /* TODO: implement layout */
+    }
+}
