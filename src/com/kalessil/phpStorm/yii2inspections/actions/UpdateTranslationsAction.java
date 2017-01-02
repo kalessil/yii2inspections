e2efbf59ce88cb1f9c436083a767fe1d7ea223bc
Vladimir Reznichenko <kalessil@gmail.com>
2017-01-02 11:11:59 +0100

    [#1] Added an action, which is not appears for some reason

---
+++ src/com/kalessil/phpStorm/yii2inspections/actions/UpdateTranslationsAction.java
@@ -0,0 +1,23 @@
+package com.kalessil.phpStorm.yii2inspections.actions;
+
+/*
+ * This file is part of the Yii2 Inspections package.
+ *
+ * Author: Vladimir Reznichenko <kalessil@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class UpdateTranslationsAction extends AnAction {
+    @Override
+    public void actionPerformed(AnActionEvent event) {
+    }
+
+    @Override
+    public void update(AnActionEvent event) {
+    }
+}
