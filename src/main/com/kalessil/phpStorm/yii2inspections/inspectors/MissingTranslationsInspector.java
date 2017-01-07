feb42992d02e52aea694f753e9865cd7c7d9005a
Vladimir Reznichenko <kalessil@gmail.com>
2017-01-07 15:11:56 +0100

    [#4] MissingTranslationsInspector: implemented (no QF,no UTs)

--- src/main/com/kalessil/phpStorm/yii2inspections/inspectors/MissingTranslationsInspector.java
+++ src/main/com/kalessil/phpStorm/yii2inspections/inspectors/MissingTranslationsInspector.java
@@ -9,14 +9,27 @@ package com.kalessil.phpStorm.yii2inspections.inspectors;
  * file that was distributed with this source code.
  */
 
+import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiFile;
+import com.intellij.util.indexing.FileBasedIndex;
 import com.jetbrains.php.lang.inspections.PhpInspection;
+import com.jetbrains.php.lang.psi.elements.ArrayCreationExpression;
+import com.jetbrains.php.lang.psi.elements.ArrayHashElement;
+import com.jetbrains.php.lang.psi.elements.PhpPsiElement;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import com.jetbrains.php.lang.psi.visitors.PhpElementVisitor;
+import com.kalessil.phpStorm.yii2inspections.codeInsight.TranslationCallsIndexer;
+import com.kalessil.phpStorm.yii2inspections.inspectors.utils.TranslationProviderUtil;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 final public class MissingTranslationsInspector extends PhpInspection {
-    private static final String messagePattern = "";
+    private static final String messagePattern = "Some message are missing: %c% in total";
 
     @NotNull
     public String getShortName() {
@@ -27,6 +40,52 @@ final public class MissingTranslationsInspector extends PhpInspection {
     @NotNull
     public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
         return new PhpElementVisitor() {
+            @Override
+            public void visitPhpArrayCreationExpression(ArrayCreationExpression expression) {
+                final PsiFile file = expression.getContainingFile();
+                if (!TranslationProviderUtil.isProvider(file)) {
+                    return;
+                }
+
+                /* extract own messages */
+                final Set<String> providedMessages = new HashSet<>();
+                for (ArrayHashElement pair : expression.getHashElements()) {
+                    final PhpPsiElement key = pair.getKey();
+                    if (key instanceof StringLiteralExpression) {
+                        providedMessages.add(((StringLiteralExpression) key).getContents());
+                    }
+                }
+                if (0 == providedMessages.size()) {
+                    return;
+                }
+
+                /* search globally for missing translations */
+                final Collection<String> usages = FileBasedIndex.getInstance()
+                        .getAllKeys(TranslationCallsIndexer.identity, expression.getProject());
+                if (usages.size() > 0) {
+                    final Set<String> missing = new HashSet<>();
+
+                    final String searchPrefix = file.getName().replaceAll("\\.php$", "|");
+                    for (String usage : usages) {
+                        if (!usage.startsWith(searchPrefix)) {
+                            continue;
+                        }
+
+                        final String message = usage.replace(searchPrefix, "");
+                        if (!providedMessages.contains(message)) {
+                            missing.add(message);
+                        }
+                    }
+                    usages.clear();
+
+                    if (missing.size() > 0) {
+                        final String message = messagePattern.replace("%c%", String.valueOf(missing.size()));
+                        holder.registerProblem(expression.getFirstChild(), message, ProblemHighlightType.WEAK_WARNING);
+                    }
+                }
+
+                providedMessages.clear();
+            }
         };
     }
 }
