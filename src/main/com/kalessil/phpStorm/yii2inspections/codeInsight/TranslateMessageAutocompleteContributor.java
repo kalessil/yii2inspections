6bd66a28d3913ab1d34eb176608e4e768936a525
Vladimir Reznichenko <kalessil@gmail.com>
2017-01-09 22:41:01 +0100

    [#4] Contributor skeleton seems to work

--- src/main/com/kalessil/phpStorm/yii2inspections/codeInsight/TranslateMessageAutocompleteContributor.java
+++ src/main/com/kalessil/phpStorm/yii2inspections/codeInsight/TranslateMessageAutocompleteContributor.java
@@ -2,22 +2,33 @@ package com.kalessil.phpStorm.yii2inspections.codeInsight;
 
 import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+import com.intellij.patterns.ElementPattern;
 import com.intellij.patterns.PlatformPatterns;
+import com.intellij.psi.PsiElement;
 import com.intellij.util.ProcessingContext;
 import com.jetbrains.php.lang.PhpLanguage;
+import com.jetbrains.php.lang.parser.PhpElementTypes;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import org.jetbrains.annotations.NotNull;
 
 public class TranslateMessageAutocompleteContributor extends CompletionContributor {
     public TranslateMessageAutocompleteContributor() {
-        extend(
-            CompletionType.BASIC,
-            PlatformPatterns.psiElement(StringLiteralExpression.class).withLanguage(PhpLanguage.INSTANCE),
-            new CompletionProvider<CompletionParameters>() {
-                public void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet resultSet) {
-                    resultSet.addElement(LookupElementBuilder.create("Hello"));
-                }
+        final CompletionProvider<CompletionParameters> provider = new CompletionProvider<CompletionParameters>() {
+            @Override
+            protected void addCompletions(@NotNull CompletionParameters completionParameters, ProcessingContext processingContext, @NotNull CompletionResultSet completionResultSet) {
+                Notifications.Bus.notify(new Notification("-", "-", "suggested", NotificationType.INFORMATION));
+                completionResultSet.addElement(LookupElementBuilder.create("Hello"));
             }
-        );
+        };
+        final ElementPattern<PsiElement> filter = PlatformPatterns.psiElement(PsiElement.class)
+                .withParent(PlatformPatterns.psiElement(StringLiteralExpression.class)
+                    .withParent(PlatformPatterns.psiElement(PhpElementTypes.PARAMETER_LIST)
+                        .withParent(PlatformPatterns.psiElement(PhpElementTypes.METHOD_REFERENCE))))
+                .withLanguage(PhpLanguage.INSTANCE);
+
+        extend(CompletionType.BASIC, filter, provider);
     }
 }
