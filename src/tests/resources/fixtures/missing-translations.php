c8661451620d0c7da649724ba059516c89d0e7b7
Vladimir Reznichenko <kalessil@gmail.com>
2017-01-10 17:35:42 +0100

    [#7] TranslationCallsIndexer - add more UTs (registerTranslations)

--- src/tests/resources/fixtures/missing-translations.php
+++ src/tests/resources/fixtures/missing-translations.php
@@ -1,4 +1,6 @@
 <?php
 
     Yii::t('missing', 'Used message in PHP');
-    Yii::t('missing', 'Missing translation from PHP');
+
+    Yii::t('missing', 'Missing translation from PHP (t)');
+    $view->registerTranslations('missing', ['Missing translation from PHP (registerTranslations)']);
