514672859ac5d899583de3d4c3a8579a9677dde7
Vladimir Reznichenko <kalessil@gmail.com>
2017-01-10 17:02:45 +0100

    [#7] Code grooming - introducing TranslationCallsProcessUtil p2

--- src/main/com/kalessil/phpStorm/yii2inspections/utils/TranslationCallsProcessUtil.java
+++ src/main/com/kalessil/phpStorm/yii2inspections/utils/TranslationCallsProcessUtil.java
@@ -1,11 +1,14 @@
 package com.kalessil.phpStorm.yii2inspections.utils;
 
 import com.intellij.psi.PsiElement;
+import com.jetbrains.php.lang.psi.elements.ArrayCreationExpression;
 import com.jetbrains.php.lang.psi.elements.MethodReference;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
+import com.kalessil.phpStorm.yii2inspections.inspectors.utils.StringLiteralExtractUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /*
@@ -20,17 +23,49 @@ import java.util.Map;
 final public class TranslationCallsProcessUtil {
 
     @Nullable
-    static public ExtractionResult process(@NotNull MethodReference reference) {
-        return null;
+    static public ProcessingResult process(@NotNull MethodReference reference) {
+        final String name         = reference.getName();
+        final PsiElement[] params = reference.getParameters();
+        if (null == name || params.length < 2 || (!name.equals("t") && !(name.equals("registerTranslations")))) {
+            return null;
+        }
+
+        /* category needs to be resolved and without any injections */
+        final StringLiteralExpression categoryLiteral = StringLiteralExtractUtil.resolveAsStringLiteral(params[0]);
+        if (null == categoryLiteral || null != categoryLiteral.getFirstPsiChild()) {
+            return null;
+        }
+
+        final Map<StringLiteralExpression, PsiElement> messages = new HashMap<>();
+        if (name.equals("t")) {
+            /* 2nd argument expected to be a string literal (possible with injections) */
+            final StringLiteralExpression messageLiteral = StringLiteralExtractUtil.resolveAsStringLiteral(params[1]);
+            if (null != messageLiteral) {
+                messages.put(messageLiteral, params[1]);
+            }
+        }
+
+        if (name.equals("registerTranslations") && params[1] instanceof ArrayCreationExpression) {
+            /* 2nd argument expected to be an inline array with string literal (possible with injections) */
+            for (PsiElement child : params[1].getChildren()) {
+                PsiElement literalCandidate            = child.getFirstChild();
+                StringLiteralExpression messageLiteral = StringLiteralExtractUtil.resolveAsStringLiteral(literalCandidate);
+                if (null != messageLiteral) {
+                    messages.put(messageLiteral, literalCandidate);
+                }
+            }
+        }
+
+        return 0 == messages.size() ? null : new ProcessingResult(categoryLiteral, messages);
     }
 
-    public class ExtractionResult {
+    static public class ProcessingResult {
         @Nullable
         private StringLiteralExpression category;
         @Nullable
         private Map<StringLiteralExpression, PsiElement> messages;
 
-        ExtractionResult(@NotNull StringLiteralExpression category, @NotNull Map<StringLiteralExpression, PsiElement> messages) {
+        ProcessingResult(@NotNull StringLiteralExpression category, @NotNull Map<StringLiteralExpression, PsiElement> messages) {
             this.category = category;
             this.messages = messages;
         }
